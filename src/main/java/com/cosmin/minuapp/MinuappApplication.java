e0adea4ac9d08b9ca8bb4d2a32687ef9408d7ca7
kosm1n <cc_cosmin@hotmail.com>
2020-10-13 18:57:52 +0200

    Initial commit

---
+++ src/main/java/com/cosmin/minuapp/MinuappApplication.java
@@ -0,0 +1,13 @@
+package com.cosmin.minuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinuappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinuappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosmin/minuapp/MinuappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmin.minuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinuappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
