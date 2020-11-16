79507dd4f1344e0dae2a558b403c6f84c6a7f127
Cosmin Valer Cotirlea <cosmin-valer.cotirlea@ext.prosegur.com>
2020-11-16 11:37:13 +0100

    gcloud config

--- src/test/java/com/cosmin/minuapp/MinuappApplicationTests.java
+++ src/test/java/com/cosmin/minuapp/MinuappApplicationTests.java
@@ -2,8 +2,11 @@ package com.cosmin.minuapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootTest
+@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = MinuappApplication.class)})
 class MinuappApplicationTests {
 
 	@Test
